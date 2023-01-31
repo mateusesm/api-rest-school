aa752412aca7939218f502edc9dabfed869a5030
Mateus Macedo <mateusemanuel198@gmail.com>
2023-01-31 10:00:05 -0300

    update seeds

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,7 @@ import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-// router.get('/', loginRequired, userController.index);
+router.get('/', userController.index);
 // router.get('/:id', userController.show);
 
 router.post('/', userController.store);
