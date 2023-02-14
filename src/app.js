08e15a66803ced70a8122b5f0baa1d16d3c89861
Mateus Macedo <mateusemanuel198@gmail.com>
2023-02-14 09:43:06 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import { resolve } from 'path';
 
 import homeRoutes from './routes/homeRoutes';
@@ -13,6 +15,21 @@ import photoRoutes from './routes/photoRoutes';
 
 import './database';
 
+const whiteList = [
+  'http://localhost:5173/',
+  'http://localhost:3000/',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS!'));
+    }
+  },
+};
+
 class App {
   constructor() {
     this.app = express();
@@ -21,6 +38,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, '..', 'uploads')));
